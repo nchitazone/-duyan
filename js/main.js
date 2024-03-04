41bd49a947714f6a1ed7ca339aab7f134a680706
nchitan <mr.chisthanh@gmail.com>
2024-03-04 11:28:32 +0900

    Site updated: 2024-03-04 11:27:45

--- js/main.js
+++ js/main.js
@@ -1,4 +1,14 @@
+$(document).ready(function()
+     {
+    //const w_heigh = document.body.offsetHeight;
+    let w_heigh = $('#container').outerHeight();
+    $('#sidebar').css({"height":w_heigh});
 
+      if (document.getElementById("sidebar_first") != null){
+     $("#more").remove();
+   }
+
+});
 $(function() { 
   // open link post in new tab
    $('#articleInner #content a').each(function () {
@@ -95,82 +105,7 @@ function myFunction() {
 }
 
 
-  if (document.getElementById("sidebar_first") != null){
-    //let sidebar_h = $("#sidebar_first").outerHeight() ;
-    //console.log("have sidebar_first",sidebar_h)
-    //$('#sidebar').css({"top":-sidebar_h  -220 }); 
- // article toc
-let toc = document.getElementById('toc')
-
-if (toc != null) {
-  window.addEventListener("scroll", scrollcatelogHandler);
-
-
-  // let x = $("#sidebar_first").outerHeight();
-  // let tocPosition =x+ 300.08;
-  // let tocPosition =x+ 380; //420
-
-
-  let elem = $('#sidebar_first');    
-  let tocPosition = elem.offset().top + elem.outerHeight(true)//+30;
-
-  console.log("tocPosition" + tocPosition)
-
-  function scrollcatelogHandler(e) {
-     let event = e || window.event,
-         target = event.target || event.srcElement;
-     let scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
-     if (scrollTop > tocPosition) {
-         toc.classList.add("toc-fixed");
-
-     } else {
-         toc.classList.remove("toc-fixed");
-     }
-  }
-
-    // elem = $('#sidebar_first');
-    // var elemOffset = elem.offset().top;
-   
-    // var scrollPos = $(window).scrollTop();
-    // var wh = $(window).height();
-
-    // var bottom = elemOffset + elem.outerHeight();
-
-    // console.log(elemOffset,bottom)
-
-    // if(scrollPos > elemOffset - wh + (wh) ){
-    //   console.log("have")
-    // }else{console.log("nohave")}
-
-
-}
-
-    //delete more
-    $("#more").remove();
-   } else {
-   	//$('#sidebar').css({"top":"-3210px" }); 
-    let elem = $('#sidebar');
-    let tocPosition =  elem.outerHeight(true) + elem.offset().top//;
-    console.log(tocPosition);
-   	
-
-    // let widget_athemes_tabs_h = $(".widget_athemes_tabs").outerHeight();
-    // let widget_wrap_h = $(".widget-wrap").outerHeight();
-    // let search_h = $(".gcse-search").outerHeight();
-    // let self_adv =0;
-    // if($("#self_adv").outerHeight()){
-    // self_adv = $("#self_adv").outerHeight();
-    // }
-
-    // sidebar_h=widget_athemes_tabs_h +widget_wrap_h +search_h+self_adv+600+100 + 50
-    // console.log(widget_athemes_tabs_h,widget_wrap_h,search_h,-sidebar_h)
-    
-	
-    $('#sidebar').css({"top":-tocPosition}); 
-     $('#sidebar').css({"position":"-webkit-sticky" }); 
-    $('#sidebar').css({"position":"sticky" }); 
-
-  }
+ 
 });
 
 //sw
